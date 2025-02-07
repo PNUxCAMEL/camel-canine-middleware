2a7925134f7d7b05bb28954ea1c6c400468c2b4b
JaehoonAn <dkswogns46@gmail.com>
2025-02-07 13:09:23 +0900

    Added sllidar ros pkg

---
+++ ros_pkg/sllidar_ros2/src/setup.hpp
@@ -0,0 +1,11 @@
+//
+// Created by jh on 25. 2. 7.
+//
+
+#ifndef SETUP_HPP
+#define SETUP_HPP
+
+constexpr float MIN_BLOCK_ANGLE = 3.141592 * 5 / 4;
+constexpr float MAX_BLOCK_ANGLE = 3.141592 * 7 / 4;
+
+#endif //SETUP_HPP
